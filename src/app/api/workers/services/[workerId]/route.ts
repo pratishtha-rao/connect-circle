0956de09473c4255eb284bb0ad4568ed07c05c00
pratishtha-rao <pratishtha.srao@gmail.com>
2026-07-27 06:46:58 -0500

    Deployment

--- src/app/api/workers/services/[workerId]/route.ts
+++ src/app/api/workers/services/[workerId]/route.ts
@@ -2,17 +2,15 @@ import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { getCurrentProfile } from "@/lib/profile";
 
-type Props = {
-  params: Promise<{
-    workerId: string;
-  }>;
-};
-
 export async function POST(
   req: Request,
-  { params }: Props
+  {
+    params,
+  }: {
+    params: Promise<{ workerId: string }>;
+  }
 ) {
-  try {
+    try {
     const profile = await getCurrentProfile();
 
     if (!profile) {
